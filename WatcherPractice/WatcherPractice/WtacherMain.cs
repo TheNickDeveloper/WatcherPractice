c914b6ad3ff83cd639015af9938d6e4a4aae561c
Pochien Tsai <nick921209@gmail.com>
2018-01-06 16:12:51 +0800

    watcher_new file created event handler

---
+++ WatcherPractice/WatcherPractice/WtacherMain.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.IO;
+
+namespace WatcherPractice
+{
+    class WtacherMain
+    {
+        public static void WatchStart(string path, string filter)
+        {
+            FileSystemWatcher watcher = new FileSystemWatcher();
+            watcher.Path = path;
+
+            watcher.Filter = filter;
+
+            watcher.Changed += new FileSystemEventHandler(Onprocess);
+            watcher.Created += new FileSystemEventHandler(Onprocess);
+
+        }
+
+
+        private static void Onprocess(object source, FileSystemEventArgs e)
+        {
+            if (e.ChangeType == WatcherChangeTypes.Created)
+            {
+                Oncreated(source, e);
+            } 
+        }
+
+        private static void Oncreated(object source, FileSystemEventArgs fileSystemEventArgs)
+        {
+            Console.WriteLine("Created file event handling.");
+        }
+    }
+}
